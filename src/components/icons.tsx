7e7094c4f15bc769a36917b157f634ce0fe2427e
Javier Ramirez <adnjavierr@gmail.com>
2025-09-09 22:25:22 +0000

    utiliza este logo para ponerlo en inicio de sesión

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Image
-      src="https://i.imgur.com/3z22Z3A.png"
+      src="https://i.imgur.com/gC2nZf6.png"
       alt="UnoTI Logo"
       fill
       className="object-contain"
