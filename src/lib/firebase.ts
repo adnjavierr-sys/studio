0af252e1469858e176e017c9c135dbd17db7d18f
Javier Ramirez <adnjavierr@gmail.com>
2025-09-12 16:26:50 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration using environment variables
-// These variables are loaded from .env.local
+// These variables are loaded from .env.local by Next.js
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
