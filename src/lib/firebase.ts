a700c594979831c6251b0907ed8cdf1437a2a327
Javier Ramirez <adnjavierr@gmail.com>
2025-09-12 23:00:27 +0000

    Try fixing this error: `Console Error: [2025-09-12T22:58:55.990Z] @fireb

--- src/lib/firebase.ts
+++
@@ -1,3 +0,0 @@
-// Este archivo ya no es necesario, ya que la inicialización se maneja en el hook `use-firebase`.
-// Puedes eliminarlo de forma segura si lo deseas, o mantenerlo vacío.
-// Dejarlo vacío asegura que las importaciones existentes no fallen, pero no tendrá ninguna funcionalidad.
