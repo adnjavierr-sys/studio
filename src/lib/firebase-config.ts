73d60b0a15dacc28bd0e989d6de3319f87803975
Javier Ramirez <adnjavierr@gmail.com>
2025-09-18 18:03:14 +0000

    File changes

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -1,7 +1,7 @@
 
 // src/lib/firebase-config.ts
 import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
-import { getFirestore, type Firestore, initializeFirestore, CACHE_SIZE_UNLIMITED } from 'firebase/firestore';
+import { getFirestore, type Firestore } from 'firebase/firestore';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getStorage, type FirebaseStorage } from 'firebase/storage';
 
@@ -21,6 +21,7 @@ const firebaseConfig = {
   "apiKey": "AIzaSyDyeqPRhp8bdeGnlMayif1kmPnXEJUGR1Y",
   "authDomain": "unoti-ticket-i9spt.firebaseapp.com",
   "messagingSenderId": "529378070793",
+  "measurementId": "G-9X53BE2L9B"
 };
 
 export function initializeFirebase(): FirebaseServices | null {
@@ -34,9 +35,7 @@ export function initializeFirebase(): FirebaseServices | null {
   
   const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
   
-  const db = initializeFirestore(app, {
-    cacheSizeBytes: CACHE_SIZE_UNLIMITED,
-  });
+  const db = getFirestore(app);
 
   const auth = getAuth(app);
   const storage = getStorage(app);
