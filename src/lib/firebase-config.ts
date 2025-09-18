e4acaf0af2be447ff6158177117f9e7482827c24
Javier Ramirez <adnjavierr@gmail.com>
2025-09-18 23:10:42 +0000

    File changes

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -1,3 +1,4 @@
+
 // src/lib/firebase-config.ts
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
@@ -7,11 +8,10 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
     "projectId": "unoti-ticket-i9spt",
     "appId": "1:529378070793:web:4b753d9444c7dfbc50c3d8",
-    "storageBucket": "unoti-ticket-i9spt.appspot.com",
     "apiKey": "AIzaSyDyeqPRhp8bdeGnlMayif1kmPnXEJUGR1Y",
     "authDomain": "unoti-ticket-i9spt.firebaseapp.com",
-    "messagingSenderId": "529378070793",
-    "measurementId": "G-56V52G5Z1R"
+    "measurementId": "",
+    "messagingSenderId": "529378070793"
 };
 
 // Initialize Firebase
@@ -22,3 +22,4 @@ const storage = getStorage(app);
 
 
 export { app, db, auth, storage };
+
