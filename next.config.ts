f4d676a3fe528706750f37e518acdc8d4a68fecb
Javier Ramirez <adnjavierr@gmail.com>
2025-09-22 22:49:01 +0000

    Try fixing this error: `Console Error: Error in shopifyFetch: {}. Error

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
-        SHOPIFY_STOREFRONT_ACCESS_TOKEN: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
-    }
-};
-
-module.exports = nextConfig;
