1ce6703b4b78b06508b21228b734a3a014c0a488
Javier Ramirez <adnjavierr@gmail.com>
2025-09-22 19:37:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -38,5 +39,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-    
--- src/lib/shopify/index.ts
+++ src/lib/shopify/index.ts
@@ -1,4 +1,5 @@
 
+
 import {
   addToCartMutation,
   createCartMutation,
