646f1cc57820a10208835932ebfe35c402659f5a
Javier Ramirez <adnjavierr@gmail.com>
2025-09-22 19:09:37 +0000

    Try fixing this error: `Runtime Error: SHOPIFY_STORE_DOMAIN and SHOPIFY_

--- next.config.ts
+++ next.config.ts
@@ -31,10 +31,8 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  env: {
-    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
-    SHOPIFY_STOREFRONT_ACCESS_TOKEN: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
-  }
 };
 
 export default nextConfig;
+
+    
