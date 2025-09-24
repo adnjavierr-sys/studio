17f3b32af7c5c9ecfc26d7ddeec4aa4b2ab7e6bf
Javier Ramirez <adnjavierr@gmail.com>
2025-09-24 19:15:07 +0000

    repara este error

--- src/lib/shopify/index.ts
+++ src/lib/shopify/index.ts
@@ -20,14 +20,13 @@ import { getProductQuery } from './queries/product'
 import { getCustomerQuery } from './queries/customer'
 import { cookies } from 'next/headers'
 
-const domain = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN;
-const key = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN;
+const domain = process.env.SHOPIFY_STORE_DOMAIN;
+const key = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN;
+const endpoint = `https://${domain}/api/2024-07/graphql.json`
 
 if (!domain || !key || domain === 'your-store-name.myshopify.com') {
-  throw new Error('Las variables de entorno de Shopify no están configuradas correctamente. Por favor, actualice su archivo .env.local con las credenciales correctas.');
+  throw new Error('Las variables de entorno de Shopify no están configuradas correctamente. Por favor, actualice su archivo .env con las credenciales correctas.');
 }
-
-const endpoint = `https://${domain}/api/2024-07/graphql.json`
 
 
 type ExtractVariables<T> = T extends { variables: object } ? T['variables'] : never
