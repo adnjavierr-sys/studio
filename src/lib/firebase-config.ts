de0587083faf35e80da775ec56c07a39586ea4c5
Javier Ramirez <adnjavierr@gmail.com>
2025-09-25 20:03:18 +0000

    Try fixing this error: `Console Error: [ Server ] [2025-09-25T20:02:03.4

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    "projectId": "unoti-ticket-i9spt",
-    "appId": "1:529378070793:web:4b753d9444c7dfbc50c3d8",
-    "apiKey": "AIzaSyDyeqPRhp8bdeGnlMayif1kmPnXEJUGR1Y",
-    "authDomain": "unoti-ticket-i9spt.firebaseapp.com",
-    "storageBucket": "unoti-ticket-i9spt.appspot.com",
-    "messagingSenderId": "529378070793",
-    "measurementId": "G-9T4E21J79L"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
